6691aa7668572cec754713c556460878f405ff7a
aquariuz <gpetiaa@gmail.com>
2018-06-03 00:10:42 +0300

    commented last row

--- Arrays-and-Matrices-Lab.js
+++ Arrays-and-Matrices-Lab.js
@@ -160,4 +160,4 @@ function findNeighbours(arr){
 
 // findNeighbours([['2', '3', '4', '7', '0'], ['4', '0', '5', '3', '4'], ['2', '3', '5', '4', '2'], ['9', '8', '7', '5', '4']])
 // findNeighbours([['test', 'yes', 'yo', 'ho'], ['well', 'done', 'yo', '6'], ['not', 'done', 'yet', '5']])
-findNeighbours([[2, 2, 5, 7, 4], [4, 0, 5, 3, 4], [2, 5, 5, 4, 2]])
+// findNeighbours([[2, 2, 5, 7, 4], [4, 0, 5, 3, 4], [2, 5, 5, 4, 2]])
