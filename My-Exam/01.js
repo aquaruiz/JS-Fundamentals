61b5b2dec6bac0695a7e05b4204169a7d9181988
aquariuz <gpetiaa@gmail.com>
2018-06-15 08:19:45 +0300

    fixed task 1 for 100/100

--- My-Exam/01.js
+++ My-Exam/01.js
@@ -35,8 +35,9 @@ function planTravel(input){
             continue
         }
 
-        let [profession, offeredGold] = row.split(" : ").map(e => e.trim())
+        let [profession, offeredGold] = row.split(":").map(e => e.trim())
         offeredGold = Number(offeredGold)
+        let gold = 0
         
         if (specializedProfessions.includes(profession) && offeredGold < 200) {
             continue
@@ -45,26 +46,33 @@ function planTravel(input){
         if (specializedProfessions.includes(profession)) {
             specializedCustomers++
             // spend for candies
-            offeredGold *= 0.8
+            gold = 0.8 * offeredGold
 
             if (specializedCustomers % 2 === 0) {
-                offeredGold += 200
+                gold += 200
             }
         }
 
+ 		if (averageProfessions.includes(profession)) {
+            gold = offeredGold
+        }
+        
         if (clumsyProfessions.includes(profession)) {
             clumsyCustomers++
 
-            if (clumsyCustomers % 2 === 0 && clumsyCustomers % 3 === 0){
-                offeredGold *= 0.85                
-            } else if (clumsyCustomers % 2 === 0) {
-                offeredGold *= 0.95
+            // if (clumsyCustomers % 2 === 0 && clumsyCustomers % 3 === 0){
+            //     gold = 0.85 * offeredGold               
+            // } else 
+            if (clumsyCustomers % 2 === 0) {
+                gold = 0.95 * offeredGold
             } else if (clumsyCustomers % 3 === 0) {
-                offeredGold *= 0.9
-            } 
+                gold = 0.9 * offeredGold
+            } else {
+                gold = offeredGold
+            }
         }
 
-        earnedGold += offeredGold
+        earnedGold += gold
     }    
 
     if (earnedGold < 1000) {
@@ -78,16 +86,19 @@ function planTravel(input){
 
 // planTravel(["Programming : 500", "Driving : 243", "Singing : 100", "Cooking : 199"])
 // planTravel(["Programming : 500", "Driving : 243.55", "Acting : 200", "Singing : 100", "Cooking : 199", "Hardware maintenance : 800", "Gardening : 700", "Programming : 500"])
-// planTravel(["Programming : 300",
-//     "Cooking     : 75",
-//     "Hardware maintenance : 50"])
+// planTravel(["Programming : 300", "Cooking : 75", "Hardware maintenance : 50"])
 
+// planTravel(["Programming : 500",
+//     "Driving : 243.55",
+//     "Acting : 200",
+//     "Singing : 100",
+//     "Cooking : 199",
+//     "Hardware maintenance : 800",
+//     "Gardening : 700",
+//     "Programming : 500",
+//     ''])
 planTravel(["Programming : 500",
-    "Driving : 243.55",
-    "Acting : 200",
+    "Driving : 243",
     "Singing : 100",
-    "Cooking : 199",
-    "Hardware maintenance : 800",
-    "Gardening : 700",
-    "Programming : 500",
-    ''])
+    "Cooking : 199"
+])
